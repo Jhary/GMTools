f5701eec6f11b2c8b30021455a36b19fbd131d20
Jhary <benjamin.koehn@gmx.de>
2017-12-17 17:53:54 +0100

    Change name of the mod

--- src/com/arduin/wurm/gmtools/utils/StringsENG.java
+++ src/com/arduin/wurm/gmtools/utils/StringsENG.java
@@ -5,7 +5,7 @@ public abstract class StringsENG {
 
     public static String VERSION = "1.0";
 
-    public static String ACTION_CATEGORY                    = "Server Tools";
+    public static String ACTION_CATEGORY                    = "GM Tools";
 
     public static String ACTION_NAME_CHECK_VILLAGE_RULES    = "Check deed rules";
     public static String ACTION_NAME_CHECK_INACTIVE_PLAYERS = "Check inactive players";
